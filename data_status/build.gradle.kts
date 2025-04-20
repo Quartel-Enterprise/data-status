7032de5e6195af56c4d27667854b038c2806f10f
PierreVieira <pierrevieiraggg@gmail.com>
2025-04-20 15:22:10 -0300

    Fix Maven Central repository URL

--- data_status/build.gradle.kts
+++ data_status/build.gradle.kts
@@ -91,8 +91,11 @@ publishing {
     
     repositories {
         maven {
-            name = "Central"
-            url = uri("https://central.sonatype.com/api/v1/publisher")
+            name = "OSSRH"
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            
             credentials {
                 username = System.getenv("CENTRAL_USERNAME")
                 password = System.getenv("CENTRAL_TOKEN")
